c1631e01fd5a74ac8f66a97bb032563eb1537b07
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2025-04-26 18:13:10 -0700

    app merge conflicts

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -11,17 +11,20 @@
     } from "./lib/WebsocketStore";
     import { onMount } from "svelte";
     import Matching from "./lib/Matching.svelte";
+    import L from "leaflet";
+    import { type ClientData } from "../../shared/types";
 
     type MatchInfo = string;
 
-    type Phase = "location" | "info" | "matching" | "chat";
-    let current_phase: Phase = "location";
+    type Phase = "info" | "matching" | "chat";
+    let current_phase: Phase = "info";
 
     let locs: LocationInfo | null = null;
     let contact: ContactInfo | null = null;
     let match: MatchInfo | null = null;
 
-    let external_locs: LocationInfo[] = [];
+    // object mapping strings to LocationInfo
+    let external_locs: any = [];
 
     function handleConsent() {
         sendMessage(ws, "consent", true as any);
@@ -33,13 +36,17 @@
     }
 
     function update_info(c: ContactInfo | null) {
-        contact = c;
-        next_phase();
+        // info section has submission for both map and info,
+        // so don't let it press until all info is there
+        if (locs !== null) {
+            contact = c;
+            next_phase();
+        }
     }
 
     function next_phase() {
         if (locs === null) {
-            current_phase = "location";
+            current_phase = "info";
             return;
         }
         if (contact === null) {
@@ -71,10 +78,10 @@
 
         ws.onopen = () => {
             disconnected = false;
-        }
+        };
 
         ws.onmessage = (e) => {
-            const res: { type: string; msg: string } = JSON.parse(e.data);
+            const res: { type: string; msg: any } = JSON.parse(e.data);
 
             switch (res.type) {
                 case "status":
@@ -95,6 +102,25 @@
                     chat.push(res.msg);
                     break;
 
+                case "map_update":
+                    let { type, data }: { type: string; data: ClientData } =
+                        res.msg;
+                    console.log(res.msg);
+                    let { name, from, to } = data;
+                    if (type == "add") {
+                        external_locs[name] = {
+                            from: new L.LatLng(from.latitude, from.longitude),
+                            to: new L.LatLng(to.latitude, to.longitude),
+                            from_radius: from.radius,
+                            to_radius: to.radius,
+                        };
+                    } else if (type == "remove") {
+                        delete external_locs[name];
+                    } else {
+                        alert("unknown map_update action " + type);
+                    }
+                    break;
+
                 default:
                     console.error("Unknown response type:", res.type);
             }
@@ -103,19 +129,22 @@
         ws.onerror = (err) => console.error("WebSocket error:", err);
         ws.onclose = () => {
             disconnected = true;
-        }
+        };
     }
 
     onMount(() => initWebSocket("ws://localhost:8080"));
 </script>
 
 <main style="display: flex; flex: row; height:100vh; width:100vw">
-
     {#if disconnected}
         <div class="disconnected"></div>
     {/if}
 
-    <Map {update_loc} selecting_locations={current_phase == "location"} external_selections={external_locs} />
+    <Map
+        {update_loc}
+        selecting_locations={current_phase == "info"}
+        external_selections={external_locs}
+    />
 
     <!-- sidebar -->
     <div style="width: 30vw;">
@@ -130,13 +159,13 @@
 </main>
 
 <style>
-.disconnected {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 4000;
-    background-color: rgba(1, 1, 1, 0.3);
-}
+    .disconnected {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 4000;
+        background-color: rgba(1, 1, 1, 0.3);
+    }
 </style>
